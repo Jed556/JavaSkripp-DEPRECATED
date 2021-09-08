1c0fdcdbe055f17b5143a4423029dfc745c5d022
Jed556 <jguiriba11@gmail.com>
2021-09-08 23:48:50 +0800

    Updated log checker

--- logs.bat
+++ logs.bat
@@ -1,4 +1,4 @@
 @echo off
-if not "%1" == "max" start /MAX cmd /c %0 max & exit /b
 title JavaSkripp Heroku Logs
-heroku logs -s app -a djsbot-556 -t
+if not "%1" == "max" start /MAX cmd /c logs max & exit /b
+heroku logs -s app -a djs-javaskripp -t
