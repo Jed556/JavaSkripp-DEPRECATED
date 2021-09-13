799b6924ffec7dc86e22de7b42882e4840c47fd0
Jed556 <jguiriba11@gmail.com>
2021-09-13 22:10:42 +0800

    Retry avatar.js

--- slashCommands/Guild/avatar.js
+++ slashCommands/Guild/avatar.js
@@ -24,15 +24,19 @@ module.exports = {
 	],
     
     run: async (client, interaction) => {
-        const Target = interaction.options.getUser("target")
+		try {
+			const Target = interaction.options.getUser("target")
 
-        const Response = new MessageEmbed()
-        .setColor('RANDOM')
-        .setAuthor(`${Target.tag}'s Avatar`)
-        .setImage(Target.displayAvatarURL({dynamic: true}))
-        .setFooter(`Requested by ${interaction.user.tag}`)
+			const Response = new MessageEmbed()
+			.setColor('RANDOM')
+			.setAuthor(`${Target.tag}'s Avatar`)
+			.setImage(Target.displayAvatarURL({dynamic: true}))
+			.setFooter(`Requested by ${interaction.user.tag}`)
 
-        interaction.reply({embeds: [Response], ephemeral: true});
-        console.log(`\x1b[36m [${tLog.toLocaleString("en-US")}]\x1b[0m ${Target.tag}'s Avatar`)
-   }
+			interaction.reply({embeds: [Response], ephemeral: true});
+			console.log(`\x1b[36m [${tLog.toLocaleString("en-US")}]\x1b[0m ${Target.tag}'s Avatar`)
+		}catch (e) {
+			console.log(String(e.stack).bgRed)
+   		}
+	}
 }
