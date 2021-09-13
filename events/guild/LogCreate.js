c1e6a1183576acb96553196869dac0d57df0ee97
Jed556 <jguiriba11@gmail.com>
2021-09-13 20:53:44 +0800

    Remove date log

--- events/guild/LogCreate.js
+++ events/guild/LogCreate.js
@@ -1,12 +1,11 @@
 const client = require("../../main");
-tLog = new Date();
 
 client.ws.on("MESSAGE_CREATE", async message => {
     if (message.guild || message.content && !message.author.bot) {
         const guild = client.guilds.cache.get(message.guild_id);
         const channel = client.channels.cache.get(message.channel_id);
 
-        console.log(`\x1b[36m [${tLog.toLocaleString("en-US")}]\x1b[0m Message in ${guild.name} #${channel.name} from ${message.author.username}#${message.author.discriminator}: ${message.content}`);
+        console.log(`Message in ${guild.name} #${channel.name} from ${message.author.username}#${message.author.discriminator}: ${message.content}`);
     }
 });
 client.ws.on("INTERACTION_CREATE", async interaction => {
@@ -14,5 +13,5 @@ client.ws.on("INTERACTION_CREATE", async interaction => {
     const channel = client.channels.cache.get(interaction.channel_id);
     const user = client.users.cache.get(interaction.member.user.id);
 
-    console.log(`\x1b[36m [${tLog.toLocaleString("en-US")}]\x1b[0m Command in ${guild.name} #${channel.name} from ${user.username}#${user.discriminator}: /${interaction.data.name}`)
+    console.log(`Command in ${guild.name} #${channel.name} from ${user.username}#${user.discriminator}: /${interaction.data.name}`)
 });
