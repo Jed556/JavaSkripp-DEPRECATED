971f2fca792c65b33de219d3b2677cd978b38c40
Jed556 <jguiriba11@gmail.com>
2021-09-15 01:16:53 +0800

    Added image scraper

---
+++ slashCommands/Guild/image.js
@@ -0,0 +1,38 @@
+const { Client, CommandInteraction } = require('discord.js');
+const Scraper = require('images-scraper');
+
+/**
+ *
+ * @param {Client} client
+ * @param {CommandInteraction} interaction
+ */
+
+module.exports = {
+    name: "image", //the command name for the Slash Command
+	description: "Send an image to a channel", //the command description for Slash Command Overview
+	category: "Guild",
+	cooldown: 1,
+	requiredroles: [], //Only allow specific Users with a Role to execute a Command [OPTIONAL]
+	alloweduserids: [], //Only allow specific Users to execute a Command [OPTIONAL]
+	options: [ 
+		{
+			"String": {
+				name: "search",
+				description: "Image to search",
+				required: true
+			}
+		}, 
+	],
+    run: async (client, interaction) => {
+        const google = new Scraper({
+            puppeteer: {
+                headless: true
+            }
+        })
+        
+        const imageQuery = interaction.options.getString('search')
+
+        const imageResults = google.scrape(imageQuery, 1);
+        interaction.reply(imageResults[0].url)
+    }
+}
