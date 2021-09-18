6d1eb3359a0ad97e18e37433809a3976e49eeda1
Jed556 <jguiriba11@gmail.com>
2021-09-18 16:10:57 +0800

    Test ban.js and mute.js

---
+++ slashCommands/Mod/mute.js
@@ -0,0 +1,112 @@
+const { Client, CommandInteraction, MessageEmbed } = require('discord.js');
+const ee = require("../../botconfig/embed.json");
+const ms = require('ms');
+
+/**
+*
+* @param {Client} client
+* @param {CommandInteraction} interaction
+*/
+
+module.exports = {
+    name: "mute",
+    description: "Mute a member",
+    memberpermissions: ["MANAGE_GUILD"],
+    options: [
+        {
+            "User": {
+                name: "target",
+                description: "Member to mute",
+                required: true,
+            }
+        },
+        {
+            "String": {
+                name: "reason",
+                description: "Provide a reason",
+                required: true,
+            }
+        },
+        {
+            "String": {
+                name: "preset_time",
+                description: "Preset mute duration",
+                required: false,
+                choices: [
+                    {
+                        name: "1 Hour",
+                        value: "1h"
+                    },
+                    {
+                        name: "1 Day",
+                        value: "1d"
+                    },
+                    {
+                        name: "1 Week",
+                        value: "1w"
+                    },
+                    {
+                        name: "1 Month",
+                        value: "30d"
+                    },
+                    {
+                        name: "1 Year",
+                        value: "1y"
+                    },
+                ]
+            }
+        },
+        {
+            "String": {
+                name: "reason",
+                description: "Custom mute duration (1s/1m/1h/1d/1w/1y)",
+                required: false,
+            }
+        },
+    ],
+    async execute(client, interaction) {
+        const Target = interaction.options.getMember("target");
+        const Reason = interaction.options.getString("reason") || "No reason specified";
+        const Time = interaction.options.getString("preset-time") || interaction.options.getString("time") || "1d";
+        
+        if (Target.id === interaction.member.id)
+        return interaction.reply({embeds: [
+           new MessageEmbed()
+           .setColor(ee.wrongcolor)
+           .setDescription(`You can't mute yourself`)
+        ]})
+
+        if (Target.permissions.has("MANAGE_GUILD"))
+        return interaction.reply({embeds: [
+           new MessageEmbed()
+           .setColor(ee.wrongcolor)
+           .setDescription(`You can't mute a moderator`)
+        ]})
+
+        if (!interaction.guild.roles.cache.get(process.env.MUTED_ID))
+        return interaction.reply({embeds: [
+            new MessageEmbed()
+            .setColor(ee.wrongcolor)
+            .setDescription("Mute role does not exist")
+        ]})
+
+        if (Target.roles.cache.has(process.env.MUTED_ID))
+        return interaction.reply({embeds: [
+            new MessageEmbed()
+            .setColor(ee.wrongcolor)
+            .setDescription("Member is already muted")
+        ]})
+
+        await Target.roles.add(process.env.MUTED_ID);
+        setTimeout( async () => {
+            if(!Target.roles.cache.has(process.env.MUTED_ID)) return;
+            await Target.roles.remove(process.env.MUTED_ID);
+        }, (ms(Time)))
+
+        interaction.reply({embeds: [
+            new MessageEmbed()
+            .setColor(ee.color)
+            .setDescription(`${Target} muted for ${Time}, Reason: ${Reason}`)
+        ]})
+    }
+}
