5d51a8ec1ed3b952484a3a4acf7e980dffbf8938
Jed556 <jguiriba11@gmail.com>
2021-09-18 20:04:05 +0800

    Edited reply

--- slashCommands/Info/commandcount.js
+++ slashCommands/Info/commandcount.js
@@ -37,7 +37,8 @@ module.exports = {
           .setColor(ee.color)
           .setFooter(ee.footertext, ee.footericon)
           .setTitle(`**[${client.commands.size + client.slashCommands.size + client.slashCommands.map(d => d.options).flat().length}] Total Commands Loaded**`)
-          .setDescription(`:gear: **[${client.commands.size}] Commands**\n>>>**[${client.categories.length}] Categories**\n\n:gear: **[${client.slashCommands.size + client.slashCommands.map(d => d.options).flat().length}] Slash Commands**\n>>>**[${client.slashCategories.length}] Categories**\n`)
+          .addField(`:gear: **[${client.commands.size}] Prefix Commands**`, `>>>**[${client.categories.length}] Categories**`)
+          .addField(`:gear: **[${client.slashCommands.size + client.slashCommands.map(d => d.options).flat().length}] Slash Commands**`, `>>>**[${client.slashCategories.length}] Categories**\n`)
         ]
       });
     } catch (e) {
