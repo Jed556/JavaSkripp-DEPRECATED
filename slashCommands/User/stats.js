3ffb0fb0a6bae92815e4e684ba1263cab732898b
Jed556 <jguiriba11@gmail.com>
2021-09-21 00:28:19 +0800

    Bug fix

--- slashCommands/User/stats.js
+++ slashCommands/User/stats.js
@@ -26,11 +26,11 @@ module.exports = {
             const Target = interaction.options.getUser("user")
 
             const Info = new MessageEmbed()
-                .setAuthor(`${Target.user}`, Target.user.displayAvatarURL({ dynamic: true }))
-                .setThumbnail(Target.user.displayAvatarURL({ dynamic: true }))
+                .setAuthor(`${Target.tag}`, Target.displayAvatarURL({ dynamic: true }))
+                .setThumbnail(Target.displayAvatarURL({ dynamic: true }))
                 .setColor("RANDOM")
                 .addField("Server member since", `${moment(Target.joinedAt).format('MMMM Do YYYY, h:mm:ss a')}\n**-** ${moment(Target.joinedAt).startOf('day').fromNow()}`)
-                .addField("Discord user since", `${moment(Target.user.createdAt).format('MMMM Do YYYY, h:mm:ss a')}\n**-** ${moment(Target.user.createdAt).startOf('day').fromNow()}`)
+                .addField("Discord user since", `${moment(Target.createdAt).format('MMMM Do YYYY, h:mm:ss a')}\n**-** ${moment(Target.user.createdAt).startOf('day').fromNow()}`)
 
             if (Target.roles.cache.size > 1) {
                 Info.addField(`Role${Target.roles.cache != 1 ? "s" : ""}`, `${Target.roles.cache.map(r => r).join(' ').replace("@everyone", " ")}`)
