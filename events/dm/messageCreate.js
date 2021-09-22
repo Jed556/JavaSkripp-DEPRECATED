3d15c6f0a2bb32c95d74a1efac1a89fbfa787386
Jed556 <jguiriba11@gmail.com>
2021-09-22 19:42:53 +0800

    Code cleanup

--- events/dm/messageCreate.js
+++ events/dm/messageCreate.js
@@ -2,21 +2,22 @@
 const config = require(`../../botconfig/config.json`);
 const ee = require(`../../botconfig/embed.json`);
 const settings = require(`../../botconfig/settings.json`);
-const {MessageEmbed} = require(`discord.js`);
+const { MessageEmbed } = require(`discord.js`);
 
 module.exports = async (client, message) => {
     function DM() {
         if (!message.author.bot) {
             console.log(`[${message.author.tag}] Message: ${message.content} ${message.attachments ? `\nAttachment: ${message.attachments.first().url}` : ""}`);
             client.users.fetch(settings.ownerID, false).then((user) => {
-                    user.send({ embeds: new MessageEmbed()
+                user.send({
+                    embeds: new MessageEmbed()
                         .setTimestamp()
                         .setColor(ee.color)
                         .setTitle(`**Message:**`)
                         .setDescription(message.content)
                         .setImage(message.attachments ? message.attachments.first().url : "")
                         .setAuthor(message.author.tag, message.author.avatar({ dynamic: true }))
-                    });
+                });
             });
 
             const msg = message.content.toLowerCase()
