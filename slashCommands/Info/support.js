6520ccccead03130c02a0284ebeac15c55f46163
Jed556 <jguiriba11@gmail.com>
2021-09-23 00:02:36 +0800

    Edit support embed

--- slashCommands/Info/support.js
+++ slashCommands/Info/support.js
@@ -4,26 +4,32 @@ const ee = require("../../botconfig/embed.json");
 const settings = require("../../botconfig/settings.json");
 
 module.exports = {
-  name: "support",
-  cooldown: 1,
-  description: "Sends contacts for bot support",
-  category: "Info",
-  memberpermissions: [],
-  requiredroles: [],
-  alloweduserids: [],
-  
-  run: async (client, interaction) => {
-    try {
-      const { member, channelId, guildId, applicationId, commandName,
-        deferred, replied, ephemeral, options, id, createdTimestamp } = interaction;
-    const { guild } = member;
+    name: "support",
+    cooldown: 1,
+    description: "Sends contacts for bot support",
+    category: "Info",
+    memberpermissions: [],
+    requiredroles: [],
+    alloweduserids: [],
 
-      interaction.reply({
-        ephemeral: true,
-        content: `jguiriba11@gmail.com | ${ee.owner}`
-      });
-    } catch (e) {
-      console.log(String(e.stack).bgRed)
+    run: async (client, interaction) => {
+        try {
+            const { member, channelId, guildId, applicationId, commandName,
+                deferred, replied, ephemeral, options, id, createdTimestamp } = interaction;
+            const { guild } = member;
+
+            interaction.reply({
+                ephemeral: true,
+                embeds: [new MessageEmbed()
+                    .setColor(ee.color)
+                    .setAuthor("Support", client.user.displayAvatarURL())
+                    .addField(`Discord:`, `\`${ee.owner}\`, \`${client.user.tag}\``)
+                    .addField(`Email`, `\`${ee.email}\``)
+                    .addField(`Github`, "\`[JavaSkripp](https://tiny.one/JavaSkripp-git)\`")
+                ]
+            });
+        } catch (e) {
+            console.log(String(e.stack).bgRed)
+        }
     }
-  }
 }
