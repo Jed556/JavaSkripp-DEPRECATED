bf5b7568e9a73744564f4202a81b0f079702c444
Jed556 <jguiriba11@gmail.com>
2021-09-22 19:56:02 +0800

    Bug fix

--- events/dm/messageCreate.js
+++ events/dm/messageCreate.js
@@ -7,7 +7,7 @@ const { MessageEmbed } = require(`discord.js`);
 module.exports = async (client, message) => {
     function DM() {
         if (!message.author.bot) {
-            console.log(`[${message.author.tag}] Message: ${message.content} ${message.attachments ? `\nAttachment: ${message.attachments.first().url}` : ""}`);
+            console.log(`[${message.author.tag}] Message: ${message.content} ${message.attachments.size > 0 ? `\nAttachment: ${message.attachments.first().url}` : ""}`);
             client.users.fetch(settings.ownerID, false).then((user) => {
                 user.send({
                     embeds: new MessageEmbed()
@@ -15,7 +15,7 @@ module.exports = async (client, message) => {
                         .setColor(ee.color)
                         .setTitle(`**Message:**`)
                         .setDescription(message.content)
-                        .setImage(`${message.attachments ? `${message.attachments.first().url}` : ""}`)
+                        .setImage(`${message.attachments.size > 0 ? `${message.attachments.first().url}` : ""}`)
                         .setAuthor(message.author.tag, message.author.avatar({ dynamic: true }))
                 });
             });
