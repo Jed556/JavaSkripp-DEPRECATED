daad3d55ca31dc308a2ecee228c49dfe98b3dbc1
Jed556 <jguiriba11@gmail.com>
2021-09-22 21:03:37 +0800

    Dynamic content and simplified

--- events/dm/messageCreate.js
+++ events/dm/messageCreate.js
@@ -6,15 +6,15 @@ const { MessageEmbed } = require(`discord.js`);
 
 module.exports = async (client, message) => {
     function DM() {
-        console.log(`[${message.author.tag}] Message: ${message.content} ${message.attachments.size > 0 ? `\nAttachment: ${message.attachments.first().url}` : ""}`);
+        console.log(`[${message.author.tag}] Message: ${message.content} ${message.attachments.size ? `\nAttachment: ${message.attachments.first().url}` : ""}`);
         client.users.fetch(settings.ownerID, false).then((user) => {
             user.send({
                 embeds: [new MessageEmbed()
                     .setTimestamp()
                     .setColor(ee.color)
-                    .setTitle(`**Message:**`)
-                    .setDescription(`> ${message.content}`)
-                    .setImage(`${message.attachments.size > 0 ? `${message.attachments.first().url}` : ""}`)
+                    .setTitle(`**Message from ${message.author.tag}**`)
+                    .setDescription(`${message.content ? `> ${message.content}` : ""}`)
+                    .setImage(`${message.attachments.size ? `${message.attachments.first().url}` : ""}`)
                     .setAuthor(message.author.tag, message.client.user.displayAvatarURL({ dynamic: true }))
                 ]
             });
