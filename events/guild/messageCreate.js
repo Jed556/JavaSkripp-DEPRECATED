a0fb63b09d5690200745633a47650067191f6ec0
Jed556 <jguiriba11@gmail.com>
2021-09-22 13:59:02 +0800

    Rework dm messages

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -6,7 +6,7 @@ const { onCoolDown, replacemsg } = require(`../../handlers/functions`);
 const Discord = require(`discord.js`);
 
 module.exports = async (client, message) => {
-    if (!message.guild || !message.channel || message.author.bot) return;
+    if (!message.guild || !message.channel || message.author.bot) return console.log(message.content);
     if (message.channel.partial) await message.channel.fetch();
     if (message.partial) await message.fetch();
     client.settings.ensure(message.guild.id, {
