115ded49dfbaad755cbbd7cd83be52efa264a843
Jed556 <jguiriba11@gmail.com>
2021-09-22 19:01:54 +0800

    Merge DM and message handler

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -6,7 +6,29 @@ const { onCoolDown, replacemsg } = require(`../../handlers/functions`);
 const Discord = require(`discord.js`);
 
 module.exports = async (client, message) => {
-    if (!message.guild || !message.channel || message.author.bot) return;
+    if (!message.guild || !message.channel || message.author.bot) {
+        //Direct Message handler
+        function DM() {
+            if (!message.author.bot) {
+                console.log(`[${message.author.tag}] Message: ${message.content}`);
+                client.users.fetch(settings.ownerID, false).then((user) => {
+                    if (message.attachments.size > 0) {
+                        user.send({ content: `**[${message.author.tag}] Message:** ${message.content}`, files: [message.attachments.first().url] });
+                    } else {
+                        user.send({ content: `**[${message.author.tag}] Message:** ${message.content}` });
+                    }
+                });
+
+                const msg = message.content.toLowerCase()
+                if ((msg == "hi") || (msg == "hello") || (msg == "hey")) {
+                    const replyArray = ["Yoooo!", "Hey There!", "Hello There!", "Hello Friend!", "Heyyy!"]
+                    const reply = replyArray[Math.floor(Math.random() * replyArray.length)];
+                    message.reply(reply);
+                }
+            }
+        }
+        return DM();
+    }
     if (message.channel.partial) await message.channel.fetch();
     if (message.partial) await message.fetch();
     client.settings.ensure(message.guild.id, {
