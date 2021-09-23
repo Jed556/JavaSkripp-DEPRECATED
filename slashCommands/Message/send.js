a0ba8f5014953e12b57f0d8eacad5ccdf46d66eb
Jed556 <jguiriba11@gmail.com>
2021-09-23 13:08:07 +0800

    Test send.js messaging

--- slashCommands/Message/send.js
+++ slashCommands/Message/send.js
@@ -26,15 +26,48 @@ module.exports = {
                 required: true
             }
         },
+        {
+            "String": {
+                name: "file",
+                description: "Enter file url",
+                required: true
+            }
+        },
     ],
 
     run: async (client, interaction) => {
         try {
             const Message = interaction.options.getString("message")
             const Target = interaction.options.getString("user")
-            const userID = client.users.cache.find(u => u.tag === Target).id
+            const File = interaction.options.getString("file")
+            const tag = ["#", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-            interaction.reply(userID, Message);
+            if (tag.some(v => Target.includes(v))) {
+                const userID = client.users.cache.find(u => u.tag === Target).id
+                client.users.fetch(userID, false).then((user) => {
+                    user.send({
+                        embeds: [new MessageEmbed()
+                            .setTimestamp()
+                            .setColor(ee.color)
+                            .addField(`Message:`, `${Message ? `> ${Message}` : "\u200b"}`)
+                            .setImage(`${File ? `${File}` : ""}`)
+                            .setAuthor(interaction.user.tag, interaction.user.displayAvatarURL({ dynamic: true }))
+                            .setFooter(client.user.username, client.user.displayAvatarURL())
+                        ]
+                    });
+                });
+                interaction.reply({
+                    embeds: [new MessageEmbed()
+                        .setTimestamp()
+                        .setColor(ee.color)
+                        .addField(`Sent Message:`, `${Message ? `> ${Message}` : "\u200b"}`)
+                        .setImage(`${File ? `${File}` : ""}`)
+                        .setAuthor(Target.tag, Target.displayAvatarURL({ dynamic: true }))
+                        .setFooter(client.user.username, client.user.displayAvatarURL())
+                    ], ephemeral: true
+                })
+
+            } else return interaction.reply("Please enter the user tag (ex. Gatorade#4147)");
 
         } catch (e) {
             console.log(String(e.stack).bgRed)
