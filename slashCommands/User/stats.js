ef7912501c2c6f6995925430c3bab88429bea309
Jed556 <jguiriba11@gmail.com>
2021-09-23 10:53:45 +0800

    Split Member and Target

--- slashCommands/User/stats.js
+++ slashCommands/User/stats.js
@@ -23,17 +23,18 @@ module.exports = {
 
     run: async (client, interaction) => {
         try {
-            const Target = interaction.options.getMember("user")
+            const Target = interaction.options.getUser("user")
+            const Member = interaction.options.getMember("user")
 
             const Info = new MessageEmbed()
-                .setAuthor(`${Target.tag}`, Target.member.displayAvatarURL({ dynamic: true }))
-                .setThumbnail(Target.member.displayAvatarURL({ dynamic: true }))
+                .setAuthor(`${Target.tag}`, Target.displayAvatarURL({ dynamic: true }))
+                .setThumbnail(Target.displayAvatarURL({ dynamic: true }))
                 .setColor("RANDOM")
                 .addField("Server member since", `${moment(Target.joinedAt).format('MMMM Do YYYY, h:mm:ss a')}\n**-** ${moment(Target.joinedAt).startOf('day').fromNow()}`)
                 .addField("Discord user since", `${moment(Target.createdAt).format('MMMM Do YYYY, h:mm:ss a')}\n**-** ${moment(Target.createdAt).startOf('day').fromNow()}`)
 
-            if (Target.roles.cache.size > 1) {
-                Info.addField(`Role${Target.roles.cache != 1 ? "s" : ""}`, `${Target.roles.cache.map(r => r).join(' ').replace("@everyone", " ")}`)
+            if (Member.roles.cache.size > 1) {
+                Info.addField(`Role${Member.roles.cache != 1 ? "s" : ""}`, `${Member.roles.cache.map(r => r).join(' ').replace("@everyone", " ")}`)
             } else {
                 Info.addField("Roles", "No roles to display")
             }
