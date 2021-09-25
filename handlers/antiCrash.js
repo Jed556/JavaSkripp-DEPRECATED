a6903c39b3a7aa80252d6a768e05f822e5c9b30f
Jed556 <jguiriba11@gmail.com>
2021-09-25 21:50:47 +0800

    Bug fix

--- handlers/antiCrash.js
+++ handlers/antiCrash.js
@@ -1,17 +1,17 @@
 const config = require(`../botconfig/config.json`);
 const ee = require(`../botconfig/embed.json`);
 const settings = require(`../botconfig/settings.json`);
-const { MessageEmbed } = require(`discord.js`);
+const { Client, MessageEmbed } = require(`discord.js`);
 module.exports.errDM = errDM;
 
-function errDM(client, reason, promise, err, origin, monitor, e) {
+function errDM(reason, promise, err, origin, monitor, e) {
     if (e) {
-        client.users.fetch(settings.ownerID, false).then((user) => {
+        Client.users.fetch(settings.ownerID, false).then((user) => {
             user.send({
                 embeds: [new MessageEmbed()
                     .setTimestamp()
                     .setColor(ee.errColor)
-                    .setAuthor("antiCrash.js", client.user.displayAvatarURL())
+                    .setAuthor("antiCrash.js", Client.user.displayAvatarURL())
                     .setTitle("Command Error")
                     .setDescription(`**Error:**\`\`\`${String(e.stack)}\`\`\``)
                     .setFooter("Check logs for more details")
@@ -19,12 +19,12 @@ function errDM(client, reason, promise, err, origin, monitor, e) {
             });
         });
     } else if (promise) {
-        client.users.fetch(settings.ownerID, false).then((user) => {
+        Client.users.fetch(settings.ownerID, false).then((user) => {
             user.send({
                 embeds: [new MessageEmbed()
                     .setTimestamp()
                     .setColor(ee.errColor)
-                    .setAuthor("antiCrash.js", client.user.displayAvatarURL())
+                    .setAuthor("antiCrash.js", Client.user.displayAvatarURL())
                     .setTitle("Unhandled Rejection/Catch")
                     .setDescription(`**Reason:**\`\`\`${reason}\`\`\`\n**Promise:**\`\`\`${promise}\`\`\``)
                     .setFooter("Check logs for more details")
@@ -32,12 +32,12 @@ function errDM(client, reason, promise, err, origin, monitor, e) {
             });
         });
     } else if (monitor) {
-        client.users.fetch(settings.ownerID, false).then((user) => {
+        Client.users.fetch(settings.ownerID, false).then((user) => {
             user.send({
                 embeds: [new MessageEmbed()
                     .setTimestamp()
                     .setColor(ee.errColor)
-                    .setAuthor("antiCrash.js", client.user.displayAvatarURL())
+                    .setAuthor("antiCrash.js", Client.user.displayAvatarURL())
                     .setTitle("Uncaught Exception/Catch (MONITOR)")
                     .setDescription(`**Error:**\`\`\`${err}\`\`\`\n**Origin:**\`\`\`${origin}\`\`\``)
                     .setFooter("Check logs for more details")
@@ -45,12 +45,12 @@ function errDM(client, reason, promise, err, origin, monitor, e) {
             });
         });
     } else if (origin) {
-        client.users.fetch(settings.ownerID, false).then((user) => {
+        Client.users.fetch(settings.ownerID, false).then((user) => {
             user.send({
                 embeds: [new MessageEmbed()
                     .setTimestamp()
                     .setColor(ee.errColor)
-                    .setAuthor("antiCrash.js", client.user.displayAvatarURL())
+                    .setAuthor("antiCrash.js", Client.user.displayAvatarURL())
                     .setTitle("Uncaught Exception/Catch")
                     .setDescription(`**Error:**\`\`\`${err}\`\`\`\n**Origin:**\`\`\`${origin}\`\`\``)
                     .setFooter("Check logs for more details")
@@ -58,12 +58,12 @@ function errDM(client, reason, promise, err, origin, monitor, e) {
             });
         });
     } else {
-        client.users.fetch(settings.ownerID, false).then((user) => {
+        Client.users.fetch(settings.ownerID, false).then((user) => {
             user.send({
                 embeds: [new MessageEmbed()
                     .setTimestamp()
                     .setColor(ee.errColor)
-                    .setAuthor("antiCrash.js", client.user.displayAvatarURL())
+                    .setAuthor("antiCrash.js", Client.user.displayAvatarURL())
                     .setTitle("Multiple Resolves")
                     .setDescription(`**Type:**\`\`\`${type}\`\`\`\n**Promise:**\`\`\`${promise}\`\`\`\n**Reason:**\`\`\`${reason}\`\`\``)
                     .setFooter("Check logs for more details")
@@ -73,7 +73,7 @@ function errDM(client, reason, promise, err, origin, monitor, e) {
     }
 }
 
-module.exports = async (client) => {
+module.exports = async (Client) => {
 
     process.on('unhandledRejection', (reason, promise) => {
         console.log('[antiCrash] :: Unhandled Rejection/Catch');
