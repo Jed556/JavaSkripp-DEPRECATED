d20da0613310468bd1de2bae698a0dc650a81502
Jed556 <jguiriba11@gmail.com>
2021-09-26 12:40:28 +0800

    Bug fix

--- events/listener/interactionCreate.js
+++ events/listener/interactionCreate.js
@@ -5,22 +5,21 @@ const settings = require(`../../botconfig/settings.json`);
 const { onCoolDown, replacemsg } = require("../../handlers/functions");
 const Discord = require("discord.js");
 module.exports = (client, interaction) => {
-    const CategoryName = interaction.commandName;
-    const SubCommand = interaction.options.getSubcommand();
-
-    if (interaction.guildId == null) return [interaction.reply({
-        embeds: [
-            new Discord.MessageEmbed()
-                .setColor(ee.errColor)
-                .setFooter(client.user.username, client.user.displayAvatarURL())
-                .setDescription(`**HEY! You can't execute commands in a DM.**`)
-        ]
-    }), console.log(`[${interaction.user.tag}] Command: /${CategoryName} ${SubCommand}`)]
-
-    const guild = interaction.guild.name;
-    const channel = interaction.channel.name;
     try {
-        console.log(`[${guild} in #${channel} from ${interaction.user.tag}] Command: /${CategoryName} ${SubCommand}`);
-    } catch { }
+        const CategoryName = interaction.commandName;
+        const SubCommand = interaction.options.getSubcommand();
 
+        if (interaction.guildId == null) return [interaction.reply({
+            embeds: [
+                new Discord.MessageEmbed()
+                    .setColor(ee.errColor)
+                    .setFooter(client.user.username, client.user.displayAvatarURL())
+                    .setDescription(`**HEY! You can't execute commands in a DM.**`)
+            ]
+        }), console.log(`[${interaction.user.tag}] Command: /${CategoryName} ${SubCommand}`)]
+
+        const guild = interaction.guild.name;
+        const channel = interaction.channel.name;
+            console.log(`[${guild} in #${channel} from ${interaction.user.tag}] Command: /${CategoryName} ${SubCommand}`);
+    } catch { }
 }
