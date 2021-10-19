c19ae860ce4fd49848008fe0a4154909d898b351
Jed556 <jguiriba11@gmail.com>
2021-10-19 14:13:21 +0800

    Bug fix

--- slashCommands/Moderation/clear.js
+++ slashCommands/Moderation/clear.js
@@ -95,10 +95,17 @@ module.exports = {
                         setTimeout(async () => {
                             try {
                                 await channel.bulkDelete(100, true).then(msgs => {
-                                    interaction.reply({
-                                        embeds: [embed
-                                            .setDescription(`**Deleted ${msgs.size} messages in ${channel}** \`Loop: ${iBulk + 1}]\``)]
-                                    })
+                                    if (iBulk === 0) {
+                                        interaction.reply({
+                                            embeds: [embed
+                                                .setDescription(`**Deleted ${msgs.size} messages in ${channel}** \`Loop: [${iBulk + 1}]\``)]
+                                        })
+                                    } else {
+                                        interaction.followUp({
+                                            embeds: [embed
+                                                .setDescription(`**Deleted ${msgs.size} messages in ${channel}** \`Loop: [${iBulk + 1}]\``)]
+                                        })
+                                    }
                                     setTimeout(async () => {
                                         try { await interaction.deleteReply() } catch { }
                                     }, time);
