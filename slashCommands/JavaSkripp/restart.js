a17591dd5b67095e58637da4a6a4931712b33231
Jed556 <jguiriba11@gmail.com>
2021-10-28 13:48:29 +0800

    Checks for host

--- slashCommands/JavaSkripp/restart.js
+++ slashCommands/JavaSkripp/restart.js
@@ -5,6 +5,7 @@ const settings = require("../../botconfig/settings.json");
 const moment = require("moment");
 const { errDM } = require("../../handlers/functions");
 var Heroku = require("heroku-client");
+let os = require("os");
 var token = config.herokuToken;
 var appName = config.herokuApp;
 var dynoName = config.herokuDyno;
@@ -29,40 +30,50 @@ module.exports = {
     run: async (client, interaction) => {
         try {
             const cd = interaction.options.getInteger("countdown");
-            if (cd) {
-                interaction.reply({
-                    embeds: [new MessageEmbed()
-                        .setTimestamp()
-                        .setColor(ee.color)
-                        .setAuthor("HEROKU | restart.js", ee.heroku)
-                        .setDescription(`**Restarting Host in ${cd} secs**`)
-                        .setFooter(client.user.username, client.user.displayAvatarURL())
-                    ], ephemeral: true
-                })
-                setTimeout(async () => {
+            if (os.hostname() != "Jed556")
+                if (cd) {
+                    interaction.reply({
+                        embeds: [new MessageEmbed()
+                            .setTimestamp()
+                            .setColor(ee.color)
+                            .setAuthor("HEROKU | restart.js", ee.heroku)
+                            .setDescription(`**Restarting Host in ${cd} secs**`)
+                            .setFooter(client.user.username, client.user.displayAvatarURL())
+                        ], ephemeral: true
+                    })
+                    setTimeout(async () => {
+                        try {
+                            var heroku = new Heroku({ token: token });
+                            heroku.delete('/apps/' + appName + '/dynos/' + dynoName)
+                                .then(x => console.log(x));
+                        } catch { }
+                    }, cd * 1000);
+                }
+                else {
+                    interaction.reply({
+                        embeds: [new MessageEmbed()
+                            .setTimestamp()
+                            .setColor(ee.color)
+                            .setAuthor("HEROKU | restart.js", ee.heroku)
+                            .setDescription("**Restarting Host...**")
+                            .setFooter(client.user.username, client.user.displayAvatarURL())
+                        ], ephemeral: true
+                    })
                     try {
                         var heroku = new Heroku({ token: token });
                         heroku.delete('/apps/' + appName + '/dynos/' + dynoName)
                             .then(x => console.log(x));
                     } catch { }
-                }, cd * 1000);
-            }
-            else {
-                interaction.reply({
+                }
+                else interaction.reply({
                     embeds: [new MessageEmbed()
                         .setTimestamp()
-                        .setColor(ee.color)
+                        .setColor(ee.errColor)
                         .setAuthor("HEROKU | restart.js", ee.heroku)
-                        .setDescription("**Restarting Host...**")
+                        .setDescription("**JavaSkripp is currently not hosted by Heroku**")
                         .setFooter(client.user.username, client.user.displayAvatarURL())
                     ], ephemeral: true
                 })
-                try {
-                    var heroku = new Heroku({ token: token });
-                    heroku.delete('/apps/' + appName + '/dynos/' + dynoName)
-                        .then(x => console.log(x));
-                } catch { }
-            }
         } catch (e) {
             console.log(String(e.stack).bgRed)
             errDM(client, e)
