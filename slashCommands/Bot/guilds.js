ed88f05ad3308dabb69a5bc427f7612472fdc32b
Jed556 <jguiriba11@gmail.com>
2021-11-29 19:18:27 +0800

    Add guild list

---
+++ slashCommands/Bot/guilds.js
@@ -0,0 +1,35 @@
+const { MessageEmbed } = require("discord.js");
+const Discord = require("discord.js");
+const config = require("../../botconfig/config.json");
+const ee = require("../../botconfig/embed.json");
+const settings = require("../../botconfig/settings.json");
+let cpuStat = require("cpu-stat");
+let os = require("os");
+const { errDM } = require("../../handlers/functions");
+
+module.exports = {
+    name: "guilds",
+    cooldown: 1,
+    description: "Displays guilds that JavaSkripp listen to",
+    category: "Bot",
+    memberpermissions: [],
+    requiredroles: [],
+    alloweduserids: [],
+
+    run: async (client, interaction) => {
+        try {
+            const Response = new MessageEmbed()
+                .setColor(ee.color)
+                .setTitle("Guilds")
+                .addField("Total Guilds:",  `\`${client.guilds.cache.size}\``)
+                .addField("Guild Names:", `${client.guilds.cache.map(g => `• ${g.name}`).join(`\n`)}`)
+                .setFooter(client.user.username, client.user.displayAvatarURL())
+                .setTimestamp()
+
+            interaction.reply({ embeds: [Response], ephermeral: true });
+        } catch (e) {
+            console.log(String(e.stack).bgRed)
+            errDM(client, e)
+        }
+    }
+}
